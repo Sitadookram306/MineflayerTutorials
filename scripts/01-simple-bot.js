169dcd8ef011c24c76e45edd6b66dccf7841a05f
Sitadookram306 <133823040+Sitadookram306@users.noreply.github.com>
2023-05-16 21:07:49 -0400

    Update 01-simple-bot.js

--- scripts/01-simple-bot.js
+++ scripts/01-simple-bot.js
@@ -2,8 +2,8 @@ const mineflayer = require('mineflayer');
 
 // Setup bot connection
 const bot = mineflayer.createBot({
-  host: 'localhost',
-  port: '12345',
+  host: 'Rageelixerfan124.aternos.me',
+  port: '11855',
   username: "Hello_world",
-  version: '1.8.9'
+  version: '1.16.2'
 });
